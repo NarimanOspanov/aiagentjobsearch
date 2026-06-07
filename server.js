90dae170b77df666b980938123ffc789d582498b
Nariman <nariman.ospanov@ensek.co.uk>
2026-06-07 17:12:54 +0500

    Add redirect for remote job applications to Telegram bot

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const TELEGRAM_APPLY_URL = 'https://t.me/apply_jobs_bot?start=hire_human';
+
+app.get('/remote', (req, res) => {
+  res.redirect(302, TELEGRAM_APPLY_URL);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
